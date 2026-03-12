1b48e31acfc4cefe834805906261133ef8dd4a5e
Dave Gallant <davegallant@proton.me>
2026-03-12 19:43:26 -0400

    Update manifest with new icons

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "rfd-affiliate-stripper development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_25
+            git
+            curl
+            gnumake
+          ];
+
+          shellHook = ''
+            echo "🚀 rfd-affiliate-stripper development environment loaded"
+          '';
+        };
+      }
+    );
+}
