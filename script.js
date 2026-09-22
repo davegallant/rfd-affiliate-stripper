0c122713bcc6a1f944742f3e029a386ec316cd36
davegallant <4519234+davegallant@users.noreply.github.com>
2026-09-22 00:41:15 +0000

    Apply automatic changes

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 // @match        *://forums.redflagdeals.com/*
 // @namespace    http://tampermonkey.net/
 // @updateURL    https://raw.githubusercontent.com/davegallant/rfd-affiliate-stripper/main/script.js
-// @version      2026-09-21
+// @version      2026-09-22
 // ==/UserScript==
 
 (function() {
